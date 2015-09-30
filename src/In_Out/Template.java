ff554ca5c79f67a220f8d6dfdceb7aea1d211ee7
EBaalhuis <ebbaalhuis@gmail.com>
2015-09-30 23:41:14 +0200

    Added Template that uses Reader

---
+++ src/In_Out/Template.java
@@ -0,0 +1,19 @@
+package In_Out;
+
+import java.io.IOException;
+
+//Template for every solution.
+public class Template {
+	public static void solve() throws IOException {
+		
+	}
+	
+	public static void main(String[] args) {
+		try {
+			solve();
+		} catch (IOException e) {
+		}
+	}
+}
+
+//Add class Reader here.
