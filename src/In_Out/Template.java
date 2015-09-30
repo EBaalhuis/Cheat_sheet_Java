c9031a73f538cb0b9e2391534acd1eea17f2419d
EBaalhuis <ebbaalhuis@gmail.com>
2015-10-01 00:12:31 +0200

    Merge Reader and Template. Template now works with BufferedReader.

--- src/In_Out/Template.java
+++ src/In_Out/Template.java
@@ -1,18 +1,43 @@
 package In_Out;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 //Template for every solution.
 public class Template {
-	public static void solve() throws IOException {
 		
+	public static void main(String[] args)  throws IOException{
+		Reader.init(System.in);
 	}
 	
-	public static void main(String[] args) {
-		try {
-			solve();
-		} catch (IOException e) {
-		}
+	static class Reader {
+	    static BufferedReader reader;
+	    static StringTokenizer tokenizer;
+
+	    static void init(InputStream input) {
+	        reader = new BufferedReader(
+	                     new InputStreamReader(input) );
+	        tokenizer = new StringTokenizer("");
+	    }
+
+	    static String next() throws IOException {
+	        while ( ! tokenizer.hasMoreTokens() ) {
+	            tokenizer = new StringTokenizer(
+	                   reader.readLine() );
+	        }
+	        return tokenizer.nextToken();
+	    }
+
+	    static int nextInt() throws IOException {
+	        return Integer.parseInt( next() );
+	    }
+		
+	    static double nextDouble() throws IOException {
+	        return Double.parseDouble( next() );
+	    }
 	}
 }
 
