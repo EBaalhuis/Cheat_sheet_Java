2304c5367ce2977aa5cad7ed09ee83fb0ca28d2c
Erik Baalhuis <ebbaalhuis@gmail.com>
2015-10-02 14:54:33 +0200

    Dijkstra met inner classes ipv outer

--- src/Graphs/Dijkstra.java
+++ src/Graphs/Dijkstra.java
@@ -1,36 +1,42 @@
 //Find shortest path from one source to each other vertex.
 //O(E + V logV)
 package Graphs;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.*;
 
-class V implements Comparable<V> {
-	public final int name;
-	public List<E> adj;
-	public double dist = Double.POSITIVE_INFINITY;
-	public V prev;
 
-	public V(int nm) {
-		name = nm;
-		adj = new ArrayList<E>();
-	}
-
-	public int compareTo(V other) {
-		return Double.compare(dist, other.dist);
-	}
-}
-
-class E {
-	public final V end;
-	public final double w;
-
-	public E(V argEnd, double argW) {
-		end = argEnd;
-		w = argW;
-	}
-}
 
 public class Dijkstra {
 
+	static class V implements Comparable<V> {
+		public final int name;
+		public List<E> adj;
+		public double dist = Double.POSITIVE_INFINITY;
+		public V prev;
+
+		public V(int nm) {
+			name = nm;
+			adj = new ArrayList<E>();
+		}
+
+		public int compareTo(V other) {
+			return Double.compare(dist, other.dist);
+		}
+	}
+
+	static class E {
+		public final V end;
+		public final double w;
+
+		public E(V argEnd, double argW) {
+			end = argEnd;
+			w = argW;
+		}
+	}
+	
 	public static void compute(V source) {
 		source.dist = 0.;
 		PriorityQueue<V> vQue = new PriorityQueue<V>();
@@ -53,31 +59,60 @@ public class Dijkstra {
 		}
 	}
 
-//	public static void main(String[] args) {
-//		Reader.init(System.in);
-//		int n = Reader.nextInt();
-//		
-//		for (int i = 0; i < n; i++) {
-//
-//			int nV = Reader.nextInt();
-//			int nE = Reader.nextInt();
-//			int source = Reader.nextInt() - 1;
-//			V[] vertices = new V[nV];
-//			E[] edges = new E[nE];
-//
-//			for (int j = 0; j < nV; j++) {
-//				vertices[j] = new V(j);
-//			}
-//
-//			for (int j = 0; j < nE; j++) {
-//				int end = Reader.nextInt() - 1;
-//				int start = Reader.nextInt() - 1;
-//				double w = Reader.nextDouble();
-//				edges[j] = new E(vertices[end], w);
-//				vertices[start].adj.add(edges[j]);
-//			}
-//
-//			compute(vertices[source]);
-//		}
-//	}
+	public static void main(String[] args) throws IOException {
+		in.init(System.in);
+		int n = in.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+
+			int nV = in.nextInt();
+			int nE = in.nextInt();
+			int source = in.nextInt() - 1;
+			V[] vertices = new V[nV];
+			E[] edges = new E[2*nE]; //2*nE alleen bij undirected graph
+
+			for (int j = 0; j < nV; j++) {
+				vertices[j] = new V(j);
+			}
+
+			for (int j = 0; j < nE; j++) {
+				int start = in.nextInt() - 1;
+				int end = in.nextInt() - 1;
+				double w = in.nextDouble();
+				edges[2*j] = new E(vertices[end], w);
+				edges[2*j+1] = new E(vertices[start], w); //for undirected graph
+				vertices[start].adj.add(edges[2*j]);
+				vertices[end].adj.add(edges[2*j+1]); //for undirected graph
+			}
+
+			compute(vertices[source]);
+		}
+	}
+	
+	static class in {
+	    static BufferedReader reader;
+	    static StringTokenizer tokenizer;
+
+	    static void init(InputStream input) {
+	        reader = new BufferedReader(
+	                     new InputStreamReader(input) );
+	        tokenizer = new StringTokenizer("");
+	    }
+
+	    static String next() throws IOException {
+	        while ( ! tokenizer.hasMoreTokens() ) {
+	            tokenizer = new StringTokenizer(
+	                   reader.readLine() );
+	        }
+	        return tokenizer.nextToken();
+	    }
+
+	    static int nextInt() throws IOException {
+	        return Integer.parseInt( next() );
+	    }
+		
+	    static double nextDouble() throws IOException {
+	        return Double.parseDouble( next() );
+	    }
+	}
 }
