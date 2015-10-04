0378b358f3d7c9063cc6eaaadcecabb8fe8ef97f
EBaalhuis <ebbaalhuis@gmail.com>
2015-10-04 23:22:13 +0200

    Added Basics class for miscellaneous code, mostly one-liners.

---
+++ src/Basics.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+// Some miscellaneous useful methods
+public class Basics {
+
+	// Given a List<Integer>  list remove an element with value i.
+	// Note that list.remove(i) removes the element at index i.
+	int res = list.remove(Integer.valueOf(i));
+	
+	// Given a String s with a hex number, return that number as a long.
+	long number = Long.parseLong(s, 16);
+}
