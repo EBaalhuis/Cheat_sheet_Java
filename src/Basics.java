5061678392de20c04c379b2300b3e01254eeeb46
EBaalhuis <ebbaalhuis@gmail.com>
2015-10-05 00:01:49 +0200

    !

--- src/Basics.java
+++ src/Basics.java
@@ -3,10 +3,14 @@ import java.util.ArrayList;
 // Some miscellaneous useful methods
 public class Basics {
 
-	// Given a List<Integer>  list remove an element with value i.
+	// Given a List<Integer> list remove an element with value i.
 	// Note that list.remove(i) removes the element at index i.
-	int res = list.remove(Integer.valueOf(i));
-	
+	ArrayList<Integer> list = new ArrayList<Integer>();
+	int i = 0;
+	boolean res = list.remove(Integer.valueOf(i));
+
 	// Given a String s with a hex number, return that number as a long.
-	long number = Long.parseLong(s, 16);
+	String hexNr = "1a8fd0c";
+	long number = Long.parseLong(hexNr, 16);
+
 }
