867c8b24bf3667f95442abfb987c075dd8027c0b
EBaalhuis <ebbaalhuis@gmail.com>
2015-10-04 22:54:37 +0200

    Added IsPrime and cleanup

---
+++ src/In_Out/Hexadecimal.java
@@ -0,0 +1,9 @@
+package In_Out;
+
+//Some methods to use with hexadecimal (or other base) input.
+public class Hexadecimal {
+
+	//Given a String s with a hex number, return that number as a long.
+	long number = Long.parseLong(s, 16);
+	
+}
