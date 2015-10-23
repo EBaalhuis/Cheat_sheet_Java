d03b67cc4504a1decd17c29b7cd0c6117cf5296c
EBaalhuis <ebbaalhuis@gmail.com>
2015-10-23 23:57:03 +0200

    custom orderings

---
+++ src/CustomComparable.java
@@ -0,0 +1,26 @@
+import java.util.PriorityQueue;
+
+// Make a custom class with compareTo, e.g. to put in PriorityQueue.
+public class CustomComparable {
+	
+	static class test implements Comparable<test> {
+		public int x;
+		
+		test(int xArg) {
+			x = xArg;
+		}
+		
+		public int compareTo(test o) {
+			return Integer.compare(o.x, x);
+		}
+	}
+	
+	public static void main(String[] args) {
+		test a1 = new test(1);
+		test a2 = new test(2);
+		
+		PriorityQueue<test> q = new PriorityQueue<test>();
+		q.add(a1);
+		q.add(a2);
+	}
+}
---
+++ src/CustomOrder.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+// Make a custom order for existing type, e.g. int, to sort with.
+public class CustomOrder {
+
+	static class Order implements Comparator<Integer> {
+		public int compare(Integer x, Integer y) {
+			return -1 * Integer.compare(x, y);
+		}
+	}
+	
+	public static void main(String[] args) {
+
+		Integer[] arr = new Integer[4];
+		arr[0] = 1;
+		arr[1] = 2;
+		arr[2] = 5;
+		arr[3] = -3;
+
+		Arrays.sort(arr, new Order());
+	}
+}
