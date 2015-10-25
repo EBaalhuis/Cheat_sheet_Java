c655e8711467b3b34509a376bf15e3301d58bfea
EBaalhuis <ebbaalhuis@gmail.com>
2015-10-25 02:22:15 +0200

    Change gcd and extgcd to always return positve gcd

--- src/NumberTheory/Euclid_gcd.java
+++ src/NumberTheory/Euclid_gcd.java
@@ -1,22 +1,22 @@
 package NumberTheory;
 
 public class Euclid_gcd {
-	
-	//Euclid's algorithm to find gcd.
-	public static int gcd(int p, int q) {
-		return q == 0 ? p : gcd(q, p % q);
-	}
-	
-	//Extended Euclid's algorithm. 
-	//Returns array [d, a, b] such that d = gcd(p, q), ap + bq = d.
-	public static int[] extgcd(int p, int q) {
-	      if (q == 0)
-	         return new int[] { p, 1, 0 };
 
-	      int[] vals = extgcd(q, p % q);
-	      int d = vals[0];
-	      int a = vals[2];
-	      int b = vals[1] - (p / q) * vals[2];
-	      return new int[] { d, a, b };
-	   }
+	// Euclid's algorithm to find gcd.
+	public static int gcd(int p, int q) {
+		return q == 0 ? Math.abs(p) : gcd(q, p % q);
+	}
+
+	// Extended Euclid's algorithm.
+	// Returns array [d, a, b] such that d = gcd(p, q), ap + bq = d.
+	public static int[] extgcd(int p, int q) {
+		if (q == 0) {
+			return new int[] { Math.abs(p), Integer.signum(p), 0 };
+		}
+		int[] vals = extgcd(q, p % q);
+		int d = vals[0];
+		int a = vals[2];
+		int b = vals[1] - (p / q) * vals[2];
+		return new int[] { d, a, b };
+	}
 }
