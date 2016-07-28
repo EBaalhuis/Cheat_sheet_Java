a406eecbc09103e1d69d1dd8b7a87f0838578693
EBaalhuis <ebbaalhuis@gmail.com>
2016-07-28 03:59:28 +0200

    comments

--- src/Graphs/Dijkstra.java
+++ src/Graphs/Dijkstra.java
@@ -67,7 +67,7 @@ public class Dijkstra {
 			int nE = in.nextInt();
 			int source = in.nextInt() - 1;
 			V[] vertices = new V[nV];
-			E[] edges = new E[2*nE]; //2*nE alleen bij undirected graph
+			E[] edges = new E[2*nE]; // remove "2*" for directed graph
 
 			for (int j = 0; j < nV; j++) {
 				vertices[j] = new V(j);
@@ -77,10 +77,10 @@ public class Dijkstra {
 				int start = in.nextInt() - 1;
 				int end = in.nextInt() - 1;
 				double w = in.nextDouble();
-				edges[2*j] = new E(vertices[end], w);
-				edges[2*j+1] = new E(vertices[start], w); //for undirected graph
-				vertices[start].adj.add(edges[2*j]);
-				vertices[end].adj.add(edges[2*j+1]); //for undirected graph
+				edges[2*j] = new E(vertices[end], w); // remove "2*" for directed graph
+				edges[2*j+1] = new E(vertices[start], w); // remove line for directed graph
+				vertices[start].adj.add(edges[2*j]); // remove "2*" for directed graph
+				vertices[end].adj.add(edges[2*j+1]); // remove line for directed graph
 			}
 
 			compute(vertices[source]);
