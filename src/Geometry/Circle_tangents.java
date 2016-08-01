357acbdf40feac0d84bc8417a9d774cb6cc37a7e
erik <ebbaalhuis@gmail.com>
2016-08-01 14:44:35 +0200

    circle stuff

---
+++ src/Geometry/Circle_tangents.java
@@ -0,0 +1,17 @@
+package Geometry;
+
+public class Circle_tangents {
+	
+	// Input two circles by [x, y, r]. Returns [phi, delta] such
+	// that the angels of the outer tangents are phi +/- delta.
+	static double[] cirleTan(double[] c1, double[] c2) {
+		double[] res = new double[2];
+		double dx = (c1[0]-c2[0]);
+		double dy = (c1[1]-c2[1]);
+		double d = Math.sqrt(dx*dx+dy*dy);
+		res[0] = Math.atan(dy/dx);
+		res[1] = Math.asin((c1[2]+c2[2])/d);
+		return res;
+	}
+	
+}
