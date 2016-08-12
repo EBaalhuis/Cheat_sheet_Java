4d5889d1b1b8492ab849d7d5b27790dae331128e
EBaalhuis <ebbaalhuis@gmail.com>
2016-08-12 07:22:19 +0200

    a

--- src/Geometry/Circle_Tangents.java
+++ src/Geometry/Circle_Tangents.java
@@ -4,7 +4,7 @@ public class Circle_Tangents {
 	
 	// Input two circles by [x, y, r]. Returns [phi, delta] such
 	// that the angels of the outer tangents are phi +/- delta.
-	static double[] cirleTan(double[] c1, double[] c2) {
+	static double[] circleTan(double[] c1, double[] c2) {
 		double[] res = new double[2];
 		double dx = (c1[0]-c2[0]);
 		double dy = (c1[1]-c2[1]);
