71abe2043a923848f4865266997ccd05a44a827f
EBaalhuis <ebbaalhuis@gmail.com>
2016-08-16 11:14:17 +0200

    a

---
+++ src/Data_Structures/Prefix_Tree.java
@@ -0,0 +1,53 @@
+package Data_Structures;
+
+public class Prefix_Tree {
+	private V root;
+
+	public Prefix_Tree() {
+		root = new V();
+	}
+
+	public void insert(String word) {
+		V p = root;
+		for (int i = 0; i < word.length(); i++) {
+			char c = word.charAt(i);
+			int index = c - 'a';
+			if (p.arr[index] == null) {
+				V temp = new V();
+				p.arr[index] = temp;
+				p = temp;
+			} else {
+				p = p.arr[index];
+			}
+		}
+		p.isEnd = true;
+	}
+
+	public V searchNode(String s) {
+		V p = root;
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			int index = c - 'a';
+			if (p.arr[index] != null) {
+				p = p.arr[index];
+			} else {
+				return null;
+			}
+		}
+
+		if (p == root) {
+			return null;
+		}
+		return p;
+	}
+	
+	public static class V {
+		V[] arr;
+		boolean isEnd;
+
+		public V() {
+			this.arr = new V[26];
+		}
+
+	}
+}
