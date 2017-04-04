6997053baf13df81686c26e7d5ac5ba895c79321
erik <erik@erik-ThinkPad-L450>
2017-04-04 18:38:27 +0200

    Add some bit operations

--- src/Misc.java
+++ src/Misc.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 // Some miscellaneous useful methods
 public class Misc {
 
+	// Fast bit operations
+	// Math.pow(2, i) <=> 1 << i
+	// Bitmask i, drop bit j: i ^ 1 << j
+	
 	// Given a List<Integer> list remove an element with value i.
 	// Note that list.remove(i) removes the element at index i.
 	ArrayList<Integer> list = new ArrayList<Integer>();
