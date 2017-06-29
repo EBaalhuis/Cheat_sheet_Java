e4bb529d10e0d380f937566a345227294a9b5b8b
erik <erik@erik-ThinkPad-L450>
2017-06-30 00:56:12 +0200

    Polygon area clean up

--- src/Geometry/Polygon_Area.java
+++ src/Geometry/Polygon_Area.java
@@ -1,45 +1,15 @@
 package Geometry;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Polygon_Area {
 
 	//Find the area of any polygon in 2d. Points must be in cw or ccw order.
-	static double area(double[] xcoord, double[] ycoord) {
+	static double area(double[] x, double[] y) {
 		double res = 0;
-		for (int i = 0; i < xcoord.length - 1; i++) {
-			res += xcoord[i] * ycoord[i + 1] - xcoord[i + 1] * ycoord[i];
+		for (int i = 0; i < x.length - 1; i++) {
+			res += x[i] * y[i + 1] - x[i + 1] * y[i];
 		}
-		res += xcoord[xcoord.length-1] * ycoord[0] - xcoord[0] * ycoord[xcoord.length-1];
+		res += x[x.length-1] * y[0] - x[0] * y[x.length-1];
 		return Math.abs(res/2);
 	}
 
-	static class in {
-		static BufferedReader reader;
-		static StringTokenizer tokenizer;
-
-		static void init(InputStream input) {
-			reader = new BufferedReader(new InputStreamReader(input));
-			tokenizer = new StringTokenizer("");
-		}
-
-		static String next() throws IOException {
-			while (!tokenizer.hasMoreTokens()) {
-				tokenizer = new StringTokenizer(reader.readLine());
-			}
-			return tokenizer.nextToken();
-		}
-
-		static int nextInt() throws IOException {
-			return Integer.parseInt(next());
-		}
-
-		static double nextDouble() throws IOException {
-			return Double.parseDouble(next());
-		}
-	}
 }
