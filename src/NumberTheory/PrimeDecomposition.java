c7e083a6cad268a4fcf664f5cf402e2887b06acd
erik <erik@erik-ThinkPad-L450>
2017-07-02 20:19:33 +0200

    Changed primeFactors to output HashMap instead of List

--- src/NumberTheory/PrimeDecomposition.java
+++ src/NumberTheory/PrimeDecomposition.java
@@ -1,27 +1,28 @@
 // Efficient prime factorization of integer n.
 // Returns a list of prime factors.
 package NumberTheory;
+
 import java.util.*;
 
 public class PrimeDecomposition {
-	
-	static List<Long> primeFactors(long a) {
-		List<Long> res = new ArrayList<Long>();
+
+	static HashMap<Long, Integer> primeFactors(long a) {
+		HashMap<Long, Integer> res = new HashMap<Long, Integer>();
 		while (a % 2 == 0) {
-			res.add((long) 2);
+			res.put(2L, res.get(2L) == null ? 1 : res.get(2L) + 1);
 			a = a / 2;
 		}
 		// Careful! Loop variable is a long
-		for (long i = 3; i*i <= a; i = i + 2) {
+		for (long i = 3; i * i <= a; i = i + 2) {
 			while (a % i == 0) {
-				res.add(i);
+				res.put(i, res.get(i) == null ? 1 : res.get(i) + 1);
 				a = a / i;
 			}
 		}
 		if (a > 2) {
-			res.add((long) a);
+			res.put(a, res.get(a) == null ? 1 : res.get(a) + 1);
 		}
 		return res;
 	}
-	
+
 }
