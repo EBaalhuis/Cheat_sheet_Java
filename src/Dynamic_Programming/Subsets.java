4257418340c824ab4be7131edae1779906fc55b3
erik <erik@erik-ThinkPad-L450>
2017-08-26 19:19:25 +0200

    minor

--- src/Dynamic_Programming/Subsets.java
+++ src/Dynamic_Programming/Subsets.java
@@ -1,4 +1,5 @@
 package Dynamic_Programming;
+
 import java.io.*;
 import java.util.*;
 
@@ -7,31 +8,31 @@ public class Subsets {
 	public static void main(String[] args) throws IOException {
 		IO io = new IO(System.in);
 
-			int bits = 3;
-			
-			// Top down
-		    for (int mask = (1 << (bits+1) - 1); mask >= 0; mask--) {
-		    	for (int item = 0; item < bits; item++) {
-					int newMask = mask ^ (1 << item);
-					if (newMask > mask) {
-						// Added 1 item, do things
-					}
+		int bits = 3;
+
+		// Top down
+		for (int item = 0; item < bits; item++) {
+			for (int mask = (1 << (bits + 1) - 1); mask >= 0; mask--) {
+				if ((mask & (1 << item)) == 0) {
+					int superset = mask ^ (1 << item);
+					// Do things
 				}
 			}
-		
-			// Bottom up
+		}
+
+		// Bottom up
+		for (int item = 0; item < bits; item++) {
 			for (int mask = 0; mask < (1 << bits); mask++) {
-				for (int item = 0; item < bits; item++) {
-					int ij = mask ^ (1 << item);
-					if (ij < mask) {
-						// Removed 1 item, do things
-					}
+				if ((mask & (1 << item)) != 0) {
+					int subset = mask ^ (1 << item);
+					// Do things
 				}
 			}
-			
+		}
+
 		io.close();
 	}
-	
+
 	// IO for testing
 	static class IO extends PrintWriter {
 		static BufferedReader r;
@@ -50,7 +51,7 @@ public class Subsets {
 			return t.nextToken();
 		}
 
-		public int nextInt() throws IOException{
+		public int nextInt() throws IOException {
 			return Integer.parseInt(next());
 		}
 
